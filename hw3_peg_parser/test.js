402e583df3659fd90b12b70853df2981d6174e83
Sebastian Porto <sebasporto@gmail.com>
2012-04-27 06:45:27 +1000

    Scheem parser now handles tab and returns

--- hw3_peg_parser/test.js
+++ hw3_peg_parser/test.js
@@ -5,22 +5,28 @@ var fs = require('fs'); // for loading files
 // Read file contents
 var data = fs.readFileSync('scheem.peg', 'utf-8');
 // Show the PEG grammar file
-console.log(data);
+//console.log(data);
 // Create my parser
 var parser = PEG.buildParser(data);
 // Do a test
 
-var input1 = "(+ 1 (* x 3))";
-var parsed1 = ["+", "1", ["*", "x", "3"]];
-var input2 = "(* n (factorial (- n 1)))";
-var parsed2 = ["*", "n", ["factorial", ["-", "n", "1"]]];
+var code01 = fs.readFileSync('01.scheem', 'utf-8');
+var code02 = fs.readFileSync('02.scheem', 'utf-8');
+var code03 = fs.readFileSync('03.scheem', 'utf-8');
+var code04 = fs.readFileSync('04.scheem', 'utf-8');
+var code05 = fs.readFileSync('05.scheem', 'utf-8');
+var code06 = fs.readFileSync('06.scheem', 'utf-8');
 
-assert.deepEqual( parser.parse(input1), parsed1 );
-assert.deepEqual( parser.parse(input2), parsed2 );
+var expected01 = ["+", "1", ["*", "x", "3"]];
+var expected02 = ["*", "n", ["factorial", ["-", "n", "1"]]];
+
+assert.deepEqual( parser.parse(code01), expected01 );
+assert.deepEqual( parser.parse(code02), expected02 );
 
 //whitespaces
-var input3 = "(+   1  ( *  x  3) )";
-var input4 = "(*   n ( factorial     ( - n    1))   )";
-assert.deepEqual( parser.parse(input3), parsed1 );
-assert.deepEqual( parser.parse(input4), parsed2 );
+assert.deepEqual( parser.parse(code03), expected01 );
+assert.deepEqual( parser.parse(code04), expected02 );
+//tabs and returns
+assert.deepEqual( parser.parse(code05), expected01 );
+assert.deepEqual( parser.parse(code06), expected02 );
 
